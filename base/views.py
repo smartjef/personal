2a13f34fa65339ea5dc7c6e3d56f8b8996b0732b
Jeff Odhiambo <odhiambo.jeff1@students.jkuat.ac.ke>
2022-09-20 16:50:15 +0300

    completed review section

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Review
+from .models import Review, About
+from django.contrib import messages
 # Create your views here.
 def index(request):
     reviews = Review.objects.all()
@@ -9,10 +10,34 @@ def index(request):
     return render(request,'index.html', context)
 
 def contact(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        message = request.POST['message']
+        print(first_name, last_name, email, message)
+        messages.success(request, 'Message sent successfully.')
     return render(request, 'contact.html')
 
 def about(request):
-    return render(request, 'about.html')
+    about = About.objects.all()
+    context = {
+        'about': about,
+    }
+    return render(request, 'about.html', context)
 
 def leave_review(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        position = request.POST['position']
+        msg = request.POST.get('message',"Some wonderfull review")
+
+        print(name, position, msg)
+        # Review.objects.create(
+        #     name=name,
+        #     position=position,
+        #     message=message
+        # )
+        messages.success(request, 'Review sent successfully.')
+        
     return render(request, 'leave_review.html')
