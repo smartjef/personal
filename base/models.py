902400f4b8103faa992def5c2d2741bd9f9930df
Jeff Odhiambo <odhiambo.jeff1@students.jkuat.ac.ke>
2022-09-20 09:20:35 +0300

    updates models.py in the base app

--- base/models.py
+++ base/models.py
@@ -1,3 +1,41 @@
+from turtle import position
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='reviews/', null=True, blank=True)
+    position = models.CharField(max_length=50)
+    message = models.TextField(max_length=500)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Reviews'
+        ordering = ['-date']
+
+class About(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(upload_to='about/', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'About'
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Contact'
+
