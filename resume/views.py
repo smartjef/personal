81af478a6e7c1ad3d5e59847ba0bb8cde5fef97b
Jeff Odhiambo <odhiambo.jeff1@students.jkuat.ac.ke>
2022-09-21 13:01:34 +0300

    completed modifing templates to return real data

--- resume/views.py
+++ resume/views.py
@@ -1,19 +1,27 @@
 from datetime import datetime
+import imp
 from django.shortcuts import render
 
 from resume.models import Education
+from projects_experiences.models import Project, Experience
 from skills.models import ProgrammingLanguage, Framework, Tool
 # Create your views here.
 def resume(request):
+    
     education = Education.objects.all()
     p_languages = ProgrammingLanguage.objects.all()
     frameworks = Framework.objects.all()
     tools = Tool.objects.all()
+    projects = Project.objects.all()
+    experiences = Experience.objects.all()
+
     context = {
         'education' : education,
         'p_languages' : p_languages,
         'frameworks' : frameworks,
         'tools' : tools,
+        'projects' : projects,
+        'experiences' : experiences,
     }
     return render(request, 'resume.html', context)
 
