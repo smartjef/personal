d9d1a30b46afe4daf30f0fb5f222346f48dfb2da
Jeff Odhiambo <odhiambo.jeff1@students.jkuat.ac.ke>
2022-09-22 14:18:05 +0300

    done with modules

--- resume/admin.py
+++ resume/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from .models import Education
+from .models import Education, Referees
 # Register your models here.
 
 @admin.register(Education)
 class EducationAdmin(admin.ModelAdmin):
-    list_display = ('course','institution','institution_logo','institution_url','institution_city','expected_grad_date','date_completed','certificate_url')
+    list_display = ('course','institution','institution_url','institution_city','expected_grad_date','date_completed','certificate_url')
     list_display_links = ('course',)
     search_fields = ('course', 'institution', 'institution_city', 'related_course_work')
+    list_per_page = 25
+
+@admin.register(Referees)
+class RefereesAdmin(admin.ModelAdmin):
+    list_display = ('name','company','position','email','phone','description')
+    list_display_links = ('name',)
+    search_fields = ('name', 'company', 'position', 'email', 'phone', 'description')
     list_per_page = 25
--- resume/views.py
+++ resume/views.py
@@ -1,13 +1,24 @@
-from datetime import datetime
 from django.shortcuts import render
-
-from resume.models import Education
-import datetime
+from resume.models import Education, Referees
+from skills.models import ProgrammingLanguage, Framework, Tool
+from projects_experiences.models import Project, Experience
 # Create your views here.
 def resume(request):
+
     education = Education.objects.all()
+    p_languages = ProgrammingLanguage.objects.all()
+    frameworks = Framework.objects.all()
+    tools = Tool.objects.all()
+    projects = Project.objects.all()
+    experiences = Experience.objects.all()
+    referees = Referees.objects.all()
     context = {
         'education' : education,
+        'p_languages' : p_languages,
+        'frameworks' : frameworks,
+        'tools' : tools,
+        'projects' : projects,
+        'experiences' : experiences,
+        'referees' : referees,
     }
     return render(request, 'resume.html', context)
-
