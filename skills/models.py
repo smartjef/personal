9539e09d589f0b20a68210c9502df2521bf78d6f
Jeff Odhiambo <odhiambo.jeff1@students.jkuat.ac.ke>
2022-09-22 10:56:17 +0300

    restarting models

--- skills/models.py
+++ skills/models.py
@@ -1,13 +1,37 @@
 from django.db import models
 
 # Create your models here.
+choices = (
+        ('Beginner', 'Beginner'),
+        ('Intermediate', 'Intermediate'),
+        ('Advanced', 'Advanced'),
+    )
 class ProgrammingLanguage(models.Model):
     title = models.CharField(max_length=50)
-    level = models.CharField(max_length=50)
+    level = models.CharField(max_length=50, default='Beginner', choices=choices)
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Programming Languages'
+        ordering = ['level']
 class Framework(models.Model):
     title = models.CharField(max_length=50)
-    level = models.CharField(max_length=50)
+    level = models.CharField(max_length=50, default='Beginner', choices=choices)
 
-class Tools(models.Model):
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Frameworks'
+        ordering = ['level']
+
+class Tool(models.Model):
     title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Tools'
