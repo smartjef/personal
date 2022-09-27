f7154a09f88b7d7d061a15705f801eef21fcc91d
Jeff Odhiambo <odhiambo.jeff1@students.jkuat.ac.ke>
2022-09-27 12:43:47 +0300

    Test

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4c&ohas)ic#7r*1!2_l40ww$exf9wmk4$rtxhqkc4k4_&)r=az'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
