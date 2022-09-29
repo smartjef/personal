af988c056b35d367266e454a28163f32fa91ed49
Jeff Odhiambo <odhiambo.jeff1@students.jkuat.ac.ke>
2022-09-29 16:17:53 +0300

    v1.1

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('joab/', admin.site.urls),
     path('', include('base.urls')),
     path('resume/', include('resume.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
